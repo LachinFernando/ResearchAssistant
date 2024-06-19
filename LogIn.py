1910a5501bab231efc05bf1cbc93d0f4fdbc9e69
LachinFernando <106962694+LachinFernando@users.noreply.github.com>
2024-06-19 16:52:27 +0530

    add(script):log in script

---
+++ LogIn.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+st.title("Chat Assistant LogIn")
+
+if st.session_state["login"]:
+    st.success("LogIn Successfull")
+else:
+    password = st.text_input("Please enter the password", disabled=st.session_state.login)
+    if st.button("LogIn"):
+        if not password:
+            st.error("Please Type the Password", icon = "🚨")
+        else:
+            if password == st.secrets["PASSWORD"]:
+                st.session_state.login = True
+                st.toast("Login Successful 🤩")
+                st.success("LogIn Successfull")
+                st.subheader('Please visit the chatbot page!')
+            else:
+                st.toast("Incoreect Password 😶")
+                st.error('Incorrect Password', icon = "🚨")
+
+
